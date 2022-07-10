2aa6c5d423fbae2dc4e8e82d2c77bc7c775cfb87
kozarezov <kozarezov.dd@gmail.com>
2022-07-10 20:48:57 +0300

    finish and start deploy

--- sea_shop/about/models.py
+++ sea_shop/about/models.py
@@ -1,8 +1,10 @@
+from distutils.command.upload import upload
 from django.db import models
 
 class About(models.Model):
     title = models.CharField('Заголовок', max_length=200)
     text = models.TextField('Описание', help_text='Введите текст "О нас"')
+    image = models.ImageField('Картинка', upload_to='about/', blank=True)
 
     class Meta:
         verbose_name = 'О нас'
@@ -13,5 +15,5 @@ class About(models.Model):
 
     def __iter__(self):
         for field in self._meta.fields:
-            if field.verbose_name != 'ID':
+            if field.verbose_name != 'ID' and field.verbose_name != 'Картинка':
                 yield (field.verbose_name, field.value_to_string(self))
