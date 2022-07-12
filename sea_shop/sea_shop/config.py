2d464b0de4514f39263fc72539930790df6f951e
Kozarezov <71556104+kozarezov@users.noreply.github.com>
2022-07-12 16:14:13 +0300

    Delete config.py

--- sea_shop/sea_shop/config.py
+++
@@ -1,4 +0,0 @@
-SK = 'django-insecure-glhs9r3t=n*8ad&1bi)r8#n4&pt4%)0mw5g+bz%2ejf$5)3dg$'
-BEGET_DIR_ROOT = '/home/k/kozare4m/kozare4m.beget.tech/public_html'
-BEGET_USER = 'kozare4m_django'
-BEGET_PWD = 'Sa4654022x'
